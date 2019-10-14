32111e123c621bcbdc3a20cd00947b63008afab5
Vutopa <54695094+Vutopa@users.noreply.github.com>
2019-10-13 22:14:45 -0400

    Updated

--- instantbreak.js
+++ instantbreak.js
@@ -1,6 +1,6 @@
 if(toggled.instB) {
 						if(!blocks.hasOwnProperty(Player.getPointedBlockId())) blocks[Player.getPointedBlockId()] = { friction: Block.getFriction(Player.getPointedBlockId(), Player.getPointedBlockData()), breakSpeed: Block.getDestroyTime(Player.getPointedBlockId(), Player.getPointedBlockData()), fricS: false, fricM: false, instaB: false }
-						if(!blocks[Player.getPointedBlockId()].instaB) {
+						if(!blocks[Player.getPointedBlockId()].instaB || Block.getDestroyTime(Player.getPointedBlockId()) != 0.00001) {
  blocks[Player.getPointedBlockId()].instaB = true
  Block.setDestroyTime(Player.getPointedBlockId(), 0.00001)
  }
